c8964733de5a5bfded373bc847185b6b62831989
eltiwany <alisaleh42797@gmail.com>
2022-06-27 18:20:14 +0300

    Adding welcome page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("fetch-uptime", function() {
+    return view('fetch-uptime');
+});
